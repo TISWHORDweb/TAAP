958c517c424441f3524d4a0f42969ac62b70b457
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-04-27 19:38:09 +0100

    Model creation and cleanup

---
+++ models/model.admin.js
@@ -0,0 +1,34 @@
+
+/**
+ * Model for user and admin
+ */
+const sequelize = require('../database');
+const {DataTypes, Model} = require('sequelize');
+const tableName = "taap_admin";
+/**
+ * Model extending sequelize model class
+ */
+class ModelAdmin extends Model {
+}
+
+ModelAdmin.init({
+    aid: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    email: {type: DataTypes.STRING, allowNull: false, unique: true},
+    password: {type: DataTypes.STRING, allowNull: false},
+    firstName: {type: DataTypes.STRING, allowNull: false},
+    lastName: {type: DataTypes.STRING, allowNull: false},
+    phone: {type: DataTypes.STRING, allowNull: true},
+    country: {type: DataTypes.STRING, allowNull: true},
+    balance: {type: DataTypes.INTEGER, allowNull: true, defaultValue: 0},
+    code: {type: DataTypes.INTEGER, allowNull: true, defaultValue: 0},
+    apiKey: {type: DataTypes.STRING, allowNull: true, unique: true},
+    token: {type: DataTypes.STRING, allowNull: true, unique: true},
+    lastLogin: {type: DataTypes.STRING, allowNull: true, unique: true},
+    whoIs: {type: DataTypes.INTEGER, defaultValue: 2},
+    blocked: {type: DataTypes.BOOLEAN, defaultValue: false},
+}, {sequelize, tableName});
+/**
+ * Run belonging and relationship before sync()
+ */
+sequelize.sync();
+module.exports = ModelAdmin;
--- models/model.parent.js
+++ models/model.parent.js
@@ -2,30 +2,33 @@
 /**
  * Model for user and admin
  */
-const sequelize = require('./../database');
+const sequelize = require('../database');
 const {DataTypes, Model} = require('sequelize');
-const tableName = "taap_users";
+const tableName = "taap_parent";
 /**
  * Model extending sequelize model class
  */
-class ModelUser extends Model {
+class ModelParent extends Model {
 }
 
-ModelUser.init({
-    uid: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+ModelParent.init({
+    pid: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
     email: {type: DataTypes.STRING, allowNull: false, unique: true},
     password: {type: DataTypes.STRING, allowNull: false},
-    fullname: {type: DataTypes.STRING, allowNull: false},
+    firstName: {type: DataTypes.STRING, allowNull: false},
+    lastName: {type: DataTypes.STRING, allowNull: false},
     phone: {type: DataTypes.STRING, allowNull: true},
     country: {type: DataTypes.STRING, allowNull: true},
     balance: {type: DataTypes.INTEGER, allowNull: true, defaultValue: 0},
-    status: {type: DataTypes.BOOLEAN, defaultValue: false},
+    code: {type: DataTypes.INTEGER, allowNull: true, defaultValue: 0},
     apiKey: {type: DataTypes.STRING, allowNull: true, unique: true},
     token: {type: DataTypes.STRING, allowNull: true, unique: true},
+    lastLogin: {type: DataTypes.STRING, allowNull: true, unique: true},
     whoIs: {type: DataTypes.INTEGER, defaultValue: 0},
+    blocked: {type: DataTypes.BOOLEAN, defaultValue: false},
 }, {sequelize, tableName});
 /**
  * Run belonging and relationship before sync()
  */
 sequelize.sync();
-module.exports = ModelUser;
+module.exports = ModelParent;
