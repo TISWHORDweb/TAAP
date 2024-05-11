73ab871630256ab0bab0d4922a1b144d7e86e847
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-05-11 17:16:11 +0100

    Models relationship

--- models/index.js
+++ models/index.js
@@ -5,3 +5,19 @@ exports.ModelAdmin = require('./model.admin');
 exports.ModelTransactions = require('./model.transaction');
 exports.ModelWebhook = require('./model.webhook');
 exports.ModelProgram = require('./model.program');
+exports.ModelTrx = require('./model.trx');
+exports.ModelTrxToken = require('./model.transaction.token');
+exports.ModelGlobal = require('./model.global');
+
+/**
+ * Run belonging and relationship before sync()
+ */
+require('./model.transaction').belongsTo(require("./model.school"), {
+    as: 'School',
+    foreignKey: {name: 'sid', allowNull: null}
+});
+
+require('./model.transaction').belongsTo(require("./model.parent"), {
+    as: 'Parent',
+    foreignKey: {name: 'pid', allowNull: null}
+});
