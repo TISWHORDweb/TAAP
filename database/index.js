ae258e37fe4ae904ceccc70e785aed0c7c8dcff6
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-06-15 12:22:43 +0100

    Database changed

--- database/index.js
+++ database/index.js
@@ -3,25 +3,25 @@ const MODE = process.env.MODE === "production";
 /**
  * @type {string} default server uri
  */
-const DATABASE_HOST = MODE ? process.env.DB_HOST : process.env.DEBUG_DB_HOST;
+const DATABASE_HOST = MODE ? process.env.MYSQLHOST : process.env.DEBUG_DB_HOST;
 const DATABASE_LANG = "mysql";
 /**
  * @type {string} database common name
  */
-const DATABASE_NAME = MODE ? process.env.DB_NAME : process.env.DEBUG_DB_NAME;
+const DATABASE_NAME = MODE ? process.env.MYSQL_DATABASE : process.env.DEBUG_DB_NAME;
 /**
  * @type {string} database common username
  */
-const DATABASE_USER = MODE ? process.env.DB_USER : process.env.DEBUG_DB_USER;
+const DATABASE_USER = MODE ? process.env.MYSQLUSER : process.env.DEBUG_DB_USER;
 /**
  * @type {string} database common password
  */
-const DATABASE_PASS = MODE ? process.env.DB_PASS : process.env.DEBUG_DB_PASS;
+const DATABASE_PASS = MODE ? process.env.MYSQLPASSWORD : process.env.DEBUG_DB_PASS;
 
 /**
  * @type {number} database common port
  */
-const DATABASE_PORT = MODE ? process.env.DB_PORT : process.env.DEBUG_DB_PORT;
+const DATABASE_PORT = MODE ? process.env.MYSQLPORT : process.env.DEBUG_DB_PORT;
 /**
  * Call for initialization
  */
